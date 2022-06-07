e689692503dc948b5b1bcbab5b0a8a9b7da56046
Blackharibo <Haribo90>
2022-06-07 17:07:01 +0200

    07.06. Rp: LogIn

--- src/main/java/com/example/application/views/login/LoginView.java
+++ src/main/java/com/example/application/views/login/LoginView.java
@@ -1,25 +1,39 @@
 package com.example.application.views.login;
 
-import com.vaadin.flow.component.login.LoginI18n;
-import com.vaadin.flow.component.login.LoginOverlay;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
-@PageTitle("Login")
-@Route(value = "login")
-public class LoginView extends LoginOverlay {
-    public LoginView() {
-        setAction("login");
 
-        LoginI18n i18n = LoginI18n.createDefault();
-        i18n.setHeader(new LoginI18n.Header());
-        i18n.getHeader().setTitle("TheOptimisticHR");
-        i18n.getHeader().setDescription("Login using user/user or admin/admin");
-        i18n.setAdditionalInformation(null);
-        setI18n(i18n);
+@Route("login")
+@PageTitle("Login | YOU LOGIN")
+public class LoginView extends VerticalLayout implements BeforeEnterObserver {
 
-        setForgotPasswordButtonVisible(false);
-        setOpened(true);
+    private final LoginForm login = new LoginForm();
+
+    public LoginView(){
+        addClassName("login-view");
+        setSizeFull();
+        setAlignItems(Alignment.CENTER);
+        setJustifyContentMode(JustifyContentMode.CENTER);
+
+        login.setAction("login");
+
+        add(new H1("YOU LOGIN"), login);
     }
 
+    @Override
+    //Prüft ob das die Eingabe korrekt sind, und wirft ein Fehler bei Ungültigkeit aus
+    public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
+        if(beforeEnterEvent.getLocation()
+                .getQueryParameters()
+                .getParameters()
+                .containsKey("error")) {
+            login.setError(true);
+        }
+    }
 }
