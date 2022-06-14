875d1bbd82aced73f2d3a6724d698cf206538fff
Neophutos <ben@petrolhead.de>
2022-06-14 15:35:13 +0200

    Added ConfirmDialog to ProblemManagementView (future TaskManagement)

--- src/main/java/com/example/application/views/problemmanagement/ProblemManagementView.java
+++ src/main/java/com/example/application/views/problemmanagement/ProblemManagementView.java
@@ -1,30 +1,22 @@
 package com.example.application.views.problemmanagement;
 
 import com.example.application.data.entity.Problem;
-import com.example.application.data.repository.ProblemformularRepository;
 import com.example.application.data.service.ProblemformularService;
-import com.example.application.data.service.TaskService;
 import com.example.application.views.MainLayout;
-import com.example.application.views.personformular.PersonformularView;
 import com.example.application.views.problemformular.ProblemformularView;
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.Text;
-import com.vaadin.flow.component.avatar.Avatar;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.details.Details;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Footer;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.component.virtuallist.VirtualList;
-import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.data.value.ValueChangeMode;
-import com.vaadin.flow.dom.ElementFactory;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,7 +24,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import javax.annotation.security.RolesAllowed;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
-import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @PageTitle("Problem-Management")
 @Route(value = "problem-management", layout = MainLayout.class)
@@ -40,6 +32,8 @@ import java.util.List;
 public class ProblemManagementView extends Div {
     Grid<Problem> grid = new Grid<>(Problem.class, false);
     TextField filterText = new TextField();
+    Dialog confirmDialog = new Dialog();
+
     ProblemformularService service;
 
     @Autowired
@@ -82,10 +76,37 @@ public class ProblemManagementView extends Div {
         if(problem == null) {
             Notification.show("Es wurde kein Problem ausgewählt!").addThemeVariants(NotificationVariant.LUMO_ERROR);
         } else {
-            service.delete(problem);
-            Notification.show("Problem " + problem.getId() + " wurde erfolgreich gelöst!").addThemeVariants(NotificationVariant.LUMO_SUCCESS);
-            updateList();
+            confirmDialog.setHeaderTitle("Antrag abschließen?");
+
+            Button cancelButton = createCancelButton(confirmDialog);
+            Button confirmButton = createConfirmButton(confirmDialog, problem);
+            confirmDialog.getFooter().add(cancelButton);
+            confirmDialog.getFooter().add(confirmButton);
+
+            confirmDialog.open();
         }
+    }
+
+    private Button createCancelButton(Dialog confirmDialog) {
+        Button cancelButton = new Button("Abbrechen", e -> {
+            confirmDialog.close();
+            Notification.show("Antragsbearbeitung wurde abgebrochen");
+            UI.getCurrent().getPage().reload();
+        });
+        return cancelButton;
+    }
+
+    private Button createConfirmButton(Dialog confirmDialog, Problem problem) {
+        Button saveButton = new Button("Abschließen", e -> {
+            service.delete(problem);
+            Notification.show("Antrag " + problem.getId() + " wurde erfolgreich abgeschlossen!").addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+            confirmDialog.close();
+            UI.getCurrent().getPage().reload();
+        });
+        updateList();
+        saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        return saveButton;
     }
 
 
@@ -94,7 +115,7 @@ public class ProblemManagementView extends Div {
         grid.setSizeFull();
         grid.addColumn("id");
         grid.addColumn(problem -> problem.getDatum().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM))).setHeader("Erstellungsdatum");
-        grid.addColumns("problemart","beschreibung","antragsteller");
+        grid.addColumns("problemart","beschreibung","antragstellername");
         grid.getColumns().forEach(col -> col.setAutoWidth(true));
     }
 
