50f799b6d28caa48b4ce89aec75fa47fb32e2f01
Neophutos <ben@petrolhead.de>
2022-06-23 10:06:25 +0200

    Added delete option for MitarbeiterlisteView.java

--- src/main/java/com/example/application/views/mitarbeiterliste/MitarbeiterlisteView.java
+++ src/main/java/com/example/application/views/mitarbeiterliste/MitarbeiterlisteView.java
@@ -1,13 +1,18 @@
 package com.example.application.views.mitarbeiterliste;
 
 import com.example.application.data.entity.Mitarbeiter;
+import com.example.application.data.entity.Problem;
 import com.example.application.data.service.HRService;
 import com.example.application.views.MainLayout;
 import com.example.application.views.personformular.PersonformularView;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
@@ -26,6 +31,7 @@ public class MitarbeiterlisteView extends Div {
 
     Grid<Mitarbeiter> grid = new Grid<>(Mitarbeiter.class, false);
     TextField filterText = new TextField();
+    Dialog confirmDialog = new Dialog();
     HRService service;
 
     @Autowired
@@ -52,9 +58,16 @@ public class MitarbeiterlisteView extends Div {
         filterText.addValueChangeListener(e -> updateList());
 
         Button addMitarbeiter = new Button("Mitarbeiter erstellen");
+        Button editMitarbeiter = new Button("Bearbeiten");
+        Button deleteMitarbeiter = new Button("Löschen");
 
         addMitarbeiter.addClickListener(e -> addMitarbeiter.getUI().ifPresent(ui -> ui.navigate(
                 PersonformularView.class)));
+
+        editMitarbeiter.addClickListener(e -> editMitarbeiter.getUI().ifPresent(ui -> ui.navigate(
+                PersonformularView.class)));
+
+        deleteMitarbeiter.addClickListener(e -> removeMitarbeiter(grid.asSingleSelect().getValue()));
 
         HorizontalLayout toolbar = new HorizontalLayout(filterText, addMitarbeiter);
         toolbar.addClassName("toolbar");
@@ -70,6 +83,41 @@ public class MitarbeiterlisteView extends Div {
         grid.setSizeFull();
         grid.addColumns("vorname", "nachname", "email", "position", "abteilung", "adresse");
         grid.getColumns().forEach(col -> col.setAutoWidth(true));
+    }
+
+    private void removeMitarbeiter(Mitarbeiter mitarbeiter) {
+        if(mitarbeiter == null) {
+            Notification.show("Es wurde kein Mitarbeiter ausgewählt!").addThemeVariants(NotificationVariant.LUMO_ERROR);
+        } else {
+            confirmDialog.setHeaderTitle("Mitarbeiter wirklich löschen?");
+
+            Button cancelButton = createCancelButton(confirmDialog);
+            Button confirmButton = createConfirmButton(confirmDialog, mitarbeiter);
+            confirmDialog.getFooter().add(cancelButton);
+            confirmDialog.getFooter().add(confirmButton);
+
+            confirmDialog.open();
+        }
+    }
+
+    private Button createCancelButton(Dialog confirmDialog) {
+        Button cancelButton = new Button("Abbrechen", e -> {
+            confirmDialog.close();
+            Notification.show("Löschen wurde abgebrochen");
+            updateList();
+        });
+        return cancelButton;
+    }
+
+    private Button createConfirmButton(Dialog confirmDialog, Mitarbeiter mitarbeiter) {
+        Button saveButton = new Button("Abschließen", e -> {
+            service.deleteMitarbeiter(mitarbeiter);
+            Notification.show("Antrag " + mitarbeiter.getNachname() + " wurde erfolgreich gelöscht!").addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+            confirmDialog.close();
+            updateList();
+        });
+        saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        return saveButton;
     }
 
     private void updateList(){
