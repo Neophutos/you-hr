7c648bc1ee604d49754b4393b4ed19aaf703b990
BuildTools <unconfigured@null.spigotmc.org>
2022-07-05 15:41:58 +0200

    move @PWA to another class for some reasons (hopefully it work)

---
+++ src/main/java/com/example/application/Heroku.java
@@ -0,0 +1,8 @@
+package com.example.application;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+
+@PWA(name = "TheOptimisticHR", shortName = "TheOptimisticHR", offlineResources = {})
+public class Heroku implements AppShellConfigurator {
+}
